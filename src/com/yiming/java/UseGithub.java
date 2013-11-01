753e26a90c4236a5509436bb49b7a4229535e940
ocean1208 <baiming1208@gmail.com>
2013-11-01 16:32:54 +0800

    first commit

---
+++ src/com/yiming/java/UseGithub.java
@@ -0,0 +1,13 @@
+package com.yiming.java;
+
+public class UseGithub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("use the github.");
+	}
+
+}
