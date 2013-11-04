9a454f9e1065f78f55e1419119472e2c27e9eef7
ocean1208 <baiming1208@gmail.com>
2013-11-04 18:53:38 +0800

    Update UseGithub.java

--- src/com/yiming/java/UseGithub.java
+++ src/com/yiming/java/UseGithub.java
@@ -8,6 +8,8 @@ public class UseGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("use the github.");
+		System.out.println("edit on the github web.");
+		System.out.println("write the code on the web.");
 	}
 
 }
