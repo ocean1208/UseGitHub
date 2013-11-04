d977c27e8f4ac4f2bf5002af9a09f34cd79f215d
ocean1208 <baiming1208@gmail.com>
2013-11-04 19:51:48 +0800

    commit on eclipse kepler IDE.

---
+++ src/com/yiming/java/AddFileOnKepler.java
@@ -0,0 +1,10 @@
+package com.yiming.java;
+
+public class AddFileOnKepler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("write the code on eclipse kepler IDE. upload the file to github.");
+	}
+
+}
